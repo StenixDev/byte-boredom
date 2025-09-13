70ef115d4cd95796d4f3d956245f4fa67c61baed
StenixDev <stenix.dev@gmail.com>
2025-09-13 15:10:59 +0800

    creating more components

--- src/index.js
+++ src/index.js
@@ -52,11 +52,32 @@ const pizzaData = [
 
 function App() {
  return  <div>
-  <h1>Hello World!</h1>
-  <Pizza />
-  <Pizza />
-  <Pizza />
+  <Header />
+  <Menu />
+  <Footer />
  </div>;
+}
+
+function Header(){
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu(){
+
+  return <div>
+    <h2>Our Menu</h2>
+    <Pizza />
+  <Pizza />
+  <Pizza />
+    </div>
+
+}
+
+function Footer(){
+  return <footer>
+    {
+      new Date().toLocaleDateString()
+    }. We're currently open</footer>
 }
 
 function Pizza(){
