f2e11d784f946f3d8e477412515e11b00e70c08f
StenixDev <stenix.dev@gmail.com>
2025-09-14 16:49:59 +0800

    rendering list

--- src/index.js
+++ src/index.js
@@ -70,11 +70,19 @@ function Menu(){
 
   return <main className='menu'>
     <h2>Our Menu</h2>
-    <Pizza name='Pizza Prosciutto'
+
+    <ul className='pizzas'>
+      {
+        pizzaData.map(pizza => <Pizza key={pizza.name} pizzaObj = {pizza} />)
+      }
+    </ul>
+
+
+    {/* <Pizza name='Pizza Prosciutto'
      ingredients='Tomato, mozarella, ham, aragula, and burrata cheese'
      photoName='pizzas/prosciutto.jpg'
      price={10}
-      />
+      /> */}
 
       
     </main>
@@ -83,14 +91,14 @@ function Menu(){
 
 function Pizza(props){
     
-  return <div className='pizza'>
-    <img src={props.photoName} alt={props.name} />
-    <div><h3>{props.name}</h3>
-    <p>{props.ingredients}</p>
-    <span>{props.price}</span>
+  return <li className='pizza'>
+    <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <div><h3>{props.pizzaObj.name}</h3>
+    <p>{props.pizzaObj.ingredients}</p>
+    <span>{props.pizzaObj.price}</span>
     </div>
     
-  </div>
+  </li>
 }
 
 
