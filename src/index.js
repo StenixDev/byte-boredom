2652fd1fcbc7af1788697bdf773532fcb506e1d6
StenixDev <stenix.dev@gmail.com>
2025-09-14 17:37:05 +0800

    conditional rederingg with ternaries

--- src/index.js
+++ src/index.js
@@ -69,19 +69,20 @@ function Header(){
 function Menu(){
 
   const pizzas = pizzaData
+  //const pizzas = []
   const numPizza = pizzas.length
 
   return <main className='menu'>
     <h2>Our Menu</h2>
 
     {
-      numPizza > 0 && (
+      numPizza > 0 ? (
         <ul className='pizzas'>
       {
         pizzas.map(pizza => <Pizza key={pizza.name} pizzaObj = {pizza} />)
       }
     </ul>
-      )
+      ) : (<p>We're still working on our menu. Please comeback later</p>)
     }
 
     
@@ -128,13 +129,15 @@ function Footer(){
 
   return <footer className='footer'>
     {
-      isOpen && (
+      isOpen ? (
         <div className='order'>
           <p>
         we're open until {closeHour}:00. Come visit us or oder online.
       </p>
       <button className='btn'>Order</button>
         </div>
+      ) : (
+        <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00 </p>
       )
     }
     </footer>
