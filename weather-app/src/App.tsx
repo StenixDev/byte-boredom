c3a0ab0a68a8bb9231cc06442576b84df1c52486
StenixDev <stenix.dev@gmail.com>
2025-12-14 08:27:39 +0800

    Add lucide-react dependency and update App component to use CloudSun icon

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,10 +1,15 @@
+import { CloudSun } from 'lucide-react';
+
 function App() {
   return (
     <div className='w-xl mx-auto pt-20 flex flex-col items-center justify-center'>
       <h1 className='text-3xl font-bold text'>The City</h1>
       <p>a brgy? maybe</p>
 
-      <div className=''>icon 18 deg</div>
+      <div className='text-[#68bdf2]'>
+        <CloudSun />
+        icon 18 deg
+      </div>
 
       <p>Partly Cloudy</p>
     </div>
