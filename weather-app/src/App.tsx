c6e1cd07ddc92f53bec362cda57cc210dd395e37
StenixDev <stenix.dev@gmail.com>
2025-12-18 05:40:09 +0800

    weather bottom info

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -52,6 +52,7 @@ function App() {
 latitude=${lat}&
 longitude=${lng}&
 current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,weather_code,wind_speed_10m&
+daily=sunrise,sunset&
 timezone=auto`
           );
           const data = await res.json();
@@ -59,7 +60,7 @@ timezone=auto`
 
           const temp = data?.current?.temperature_2m;
 
-          console.log(temp);
+          console.log(data);
 
           setData((prevData) => ({
             ...prevData,
@@ -102,14 +103,11 @@ timezone=auto`
     <div className='w-xl mx-auto pt-20 flex flex-col items-center justify-center'>
       <h1 className='text-3xl font-bold text'>{data?.city}</h1>
       <p>{data?.countryName}</p>
-
       <div className='text-[#68bdf2] flex my-10 items-center'>
         <CloudSun className='size-20' />
         <span className='text-7xl font-bold'>{data?.temperature}°</span>
       </div>
-
       <p className='text-xl font-bold'>{data.condition}</p>
-
       <div className='flex p-5 bg-stone-500 rounded-2xl my-5 text-[#c6c6c6] '>
         <div className='flex flex-col items-center p-4 border-r border-solid border-r-[#aeaeae]'>
           <Sun />
@@ -127,6 +125,15 @@ timezone=auto`
           <Droplet />
           <span className='font-bold'>{data?.temperature}°</span>
         </div>
+      </div>
+
+      <div className='border-5 w-xs p-5 border-stone-500 rounded-2xl my-5 text-[#c6c6c6] '>
+        <div>day / hourly temp</div>
+        <div className='flex p-3'>
+          <div className='flex-1'>x</div>
+          <div className='flex-1'>y</div>
+        </div>
+        <div>body / details</div>
       </div>
     </div>
   );
