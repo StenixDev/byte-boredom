8a565cb8f00178e482f3ac41319bb8c694b88e1a
StenixDev <stenix.dev@gmail.com>
2025-12-19 20:53:23 +0800

    adding menu and food item

---
+++ food-queue/app/components/food.tsx
@@ -0,0 +1,50 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Image from 'next/image';
+
+type FoodType = {
+  name: string;
+  price: number;
+  img: string;
+};
+
+function Food({ name, price, img }: FoodType) {
+  return (
+    <Card className='max-w-xs py-3 px-1 bg-lime-400 border-none rounded-md text-center'>
+      <CardHeader className='flex flex-col items-center'>
+        <Badge
+          className='h-5 min-w-5 rounded-full font-mono tabular-nums px-4 py-2 bg-red-600 text-white'
+          variant='outline'
+        >
+          ₱ {price}
+        </Badge>
+        <Image
+          src={img}
+          alt='Crispy fried chicken'
+          width={300}
+          height={20}
+          priority
+        />
+        <CardTitle>{name}</CardTitle>
+        <CardDescription className='text-black'>Food delicious</CardDescription>
+      </CardHeader>
+
+      <CardFooter className='flex-col gap-2'>
+        <Button
+          type='submit'
+          className='max-w-xs bg-red-600 hover:bg-red-700 cursor-pointer'
+        >
+          Add to Cart
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+export default Food;
--- food-queue/app/page.tsx
+++ food-queue/app/page.tsx
@@ -1,4 +1,19 @@
+import Food from './components/food';
+
 function Home() {
-  return <div className=''>The content</div>;
+  return (
+    <div className=''>
+      <h1 className='font-semibold text-center'>
+        <span className='inline-block bg-red-600 text-white px-5 py-1 rounded-md text-2xl'>
+          Menu
+        </span>
+      </h1>
+
+      <div className='py-5 flex gap-4'>
+        <Food name='Crispy Fried' price={120} img='/chicken.png' />
+        <Food name='Yum Burger' price={79} img='/humberger.png' />
+      </div>
+    </div>
+  );
 }
 export default Home;
