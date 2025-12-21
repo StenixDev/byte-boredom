b6722e09fbc35e079b648d4a7e55a4dd0ada89b7
StenixDev <stenix.dev@gmail.com>
2025-12-21 20:35:32 +0800

    checkout

--- food-queue/app/components/order.tsx
+++ food-queue/app/components/order.tsx
@@ -1,16 +1,53 @@
 'use client';
 import { useGlobal } from '@/context/global-state';
-import OrderDetails from './order-detail';
+
+import { Button } from '@/components/ui/button';
+
+import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+
 function Order() {
-  const { data } = useGlobal();
+  const { data, setData } = useGlobal();
+
+  const [success, setSuccess] = useState(false);
+
   const totalQuantity = data.reduce((sum, item) => {
     return sum + item.quantity * item.price;
   }, 0);
-  console.log(data);
+
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+
+  async function handleSubmit() {
+    const result = await fetch('/api/orders', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ...data, code }),
+    });
+
+    if (result) {
+      setData([]);
+      setSuccess(true);
+    }
+  }
+
+  if (success)
+    return (
+      <div className='bg-white rounded-xl p-5'>
+        <h1 className='text-2xl'>
+          Thank you for your order! We are now preparing it. Please proceed to
+          the cashier for payment.{' '}
+        </h1>
+        <h2 className='text-4xl'>
+          Your Order <span className='font-bold'>#{code}</span>
+        </h2>
+      </div>
+    );
   return (
     <div>
-      <h1 className='text-xl font-bold mb-3'>Order Datail</h1>
-
+      <h1 className='text-xl font-bold mb-3 text-center text-red-700'>
+        Order Datail
+      </h1>
       <div className='mx-auto overflow-x-auto max-w-2xl bg-white rounded-lg shadow'>
         <table className='min-w-full divide-y divide-gray-200'>
           <thead className='bg-red-600'>
@@ -58,6 +95,14 @@ function Order() {
             </tr>
           </tbody>
         </table>
+        <div className='flex'>
+          <Button
+            onClick={handleSubmit}
+            className='ml-auto mr-7 mb-5 bg-red-600 hover:bg-red-700 cursor-pointer'
+          >
+            Confirm
+          </Button>
+        </div>
       </div>
     </div>
   );
