fbacd1f9b37f4f1327f7faa27f27f5815d44cbe7
StenixDev <stenix.dev@gmail.com>
2025-12-22 18:36:10 +0800

    orders admin

--- food-queue/app/orders/page.tsx
+++ food-queue/app/orders/page.tsx
@@ -3,14 +3,26 @@
 
 import { useEffect, useState } from 'react';
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
 export default function OrdersClient() {
-  const [count, setCount] = useState(0);
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     async function fetchOrders() {
-      const res = await fetch('/api/orders');
+      const res = await fetch('http://localhost:3000/api/orders');
       const data = await res.json();
-      setCount(data.length);
+
+      setOrders(data);
     }
 
     fetchOrders(); // initial load
@@ -20,5 +32,35 @@ export default function OrdersClient() {
     return () => clearInterval(interval);
   }, []);
 
-  return <div>Total orders: {count}</div>;
+  console.log(orders);
+
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className='w-[100px]'>Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className='text-right'>Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell className='font-medium'>{order.code}</TableCell>
+            <TableCell>{order.name}</TableCell>
+            <TableCell>{order.price}</TableCell>
+            <TableCell className='text-right'>Action</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className='text-right'>$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
 }
